7bd9ee5f56f9ed1e23e6aef73a1835dc3ada52c4
Stefan Cova <s.cova@skillstar.com>
2011-12-20 11:36:10 +0100

    add boilerplate and html5.js

--- wp-content/themes/pongsari/header.php
+++ wp-content/themes/pongsari/header.php
@@ -20,6 +20,7 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link href='http://fonts.googleapis.com/css?family=Lobster+Two:400,400italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/boilerplatex.css" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
