4cc8452727e9ed0b43b66366db2ba85afa92e464
Stefan Cova <s.cova@skillstar.com>
2011-12-21 14:15:14 +0100

    re-install

--- wp-config.php
+++ wp-config.php
@@ -19,7 +19,7 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'LMV');
+define('DB_NAME', 'lmv');
 
 /** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
